9f84712e8fa8018f82d44adc3627f4c1214bdc10
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 20:11:56 +0300

    #3

---
+++ Final Practice Questions/Q3.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    int invt =23, sold=17, left;
+    float price=9.99f, tot_sal;
+    string low_level="Low Inentory", suf_level="Sufficent Inventroy", level;
+
+    left=invt-sold;
+    tot_sal=price*sold;
+
+    level=(left<10)? low_level:suf_level;
+    cout<<"Total Invintory left:" <<left<<endl;
+    cout<<"Total Sales: "<<tot_sal<<endl;
+    cout<<"Inventory Status: "<<level<<endl;
+
+    return 0;
+    
+}
