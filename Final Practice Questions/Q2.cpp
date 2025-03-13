db34d62d28a2cacd7a851fe9a3f9587adaf9373e
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 19:47:54 +0300

    #2

---
+++ Final Practice Questions/Q2.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+ 
+int main(){
+    string name="Bolbs";
+    int prod_catg= 3;
+    int qunt=3;
+    float price=9.99f;
+    int sold_items =250;
+
+    cout<<"Name of Product: "<<name<<endl;
+    cout<<"Product Catagory: "<<prod_catg<<endl;
+    cout<<"Initial Inventory Quantity: "<<qunt<<endl;
+    cout<<"Price per Unit: "<<qunt<<endl;
+    cout<<"Number of Sold: "<<sold_items<<endl;
+
+
+
+}
