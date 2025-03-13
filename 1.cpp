0461b995c6dba3426daa03c5d1269ca3bcfe828a
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 10:41:20 +0300

    Updates

---
+++ 1.cpp
@@ -0,0 +1,12 @@
+// Write a simple C++ program that includes a preprocessor directive and a main() 
+// function to print "Hello World!". Explain the purpose of the preprocessor 
+// directive used.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    cout<<"Hello World!" <<endl;
+    return 0;
+
+}
---
+++ 2.cpp
@@ -0,0 +1,19 @@
+// Write a C++ program that declares variables of different fundamental 
+// types (e.g., char, int, float, and bool) and prints their values. 
+// Explain what each type represents.
+#include<iostream>
+using namespace std;
+int main(){
+    char first='H'; //This character value assigend "first" outputs a char value of "H"
+    int num=10; //This integer value assiged "num" outputs an int value of "10"
+    float decnum=2.17f; //This float value assiged "decnum" outputs a float value of "2.17"
+    bool isTrue=false; //This boolean value assiged "isTrue" to be "False" outputs a boolean value of "0"
+
+    cout<< "Character:" <<first <<endl;
+    cout<< "Intinger:" <<num <<endl;
+    cout<< "Float:" <<decnum <<endl;
+    cout<< "Boolean:" <<isTrue<<endl;
+
+    return 0;
+
+}
