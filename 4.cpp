4356bbe89c1c990b7f2e218f6d10e566cea4f722
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 12:03:53 +0300

    Update

---
+++ 4.cpp
@@ -0,0 +1,27 @@
+// Write a program that declares one variable of each type (char, int, float,bool). 
+// Then, update each variable (for example, change the character,add 50 to the 
+// integer, multiply the float by 2, and toggle the boolean),and print the
+//  updated values.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char a='L';
+    int b=20;
+    float c=3.14f;
+    bool isTrue=true;
+
+    // updates
+    a='M'; //For the intitialized value of 'a' to be 'L' is amended to be 'M'
+    b+=30; //Same as the previous but the int value of 'b' of  20 is ammend to 50 by adding 30
+    c*=3;//This one multiplies the float vlaue of 3.14 by 3 and ammend 'c' to there product
+    isTrue=false; //This chagnes the value of isTrue from true to flase  giving out '0' 
+
+    cout<<"Chracter: " <<a <<endl;
+    cout<<"Integer: " <<b <<endl;
+    cout<<"Float: " <<c <<endl;
+    cout<<"Boolean: " <<isTrue <<endl;
+
+
+}
