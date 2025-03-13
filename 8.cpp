198db66eb9e1bba410e888e344b16f287d97d259
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 19:13:14 +0300

    Updates

---
+++ 8.cpp
@@ -0,0 +1,3 @@
+// Write a C++ program that uses an if-else statement to check if a number is positive, a
+// switch statement to handle specific values, and a for loop to print a countdown. Briefly
+// explain what each control structure does.
