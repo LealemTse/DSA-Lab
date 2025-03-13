cff8da2a0d022f7d0aaa9160200cee61a36f4435
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 21:50:32 +0300

    #9

---
+++ Final Practice Questions/Q9.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    string low_level="Low Inentory", suf_level="Sufficent Inventroy", level;
+
+    int ini_inv, new_inv, item_sold;
+    double price, tot_sold;
+    
+
+    cout<<"Enter the Initial Inventory:" <<endl;
+    cin>>ini_inv;
+    cout<<"Enter the Product Price:" <<endl;
+    cin>>price;
+    cout<<"Enter the Number of Item sold:" <<endl;
+    cin>>item_sold;
+
+    new_inv=ini_inv-=item_sold;
+    tot_sold=price*item_sold;
+
+
+
+    cout<<"New Inventory: "<<new_inv<<endl;
+    cout<<"Total Sold: "<<tot_sold<<endl;
+
+    level=(new_inv<10)? low_level:suf_level;
+    cout<<"Current Inventory Level:"<<level<<endl;
+
+    return 0;
+}
