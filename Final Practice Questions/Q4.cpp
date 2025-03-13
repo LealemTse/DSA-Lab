c0321963b25e08bc79ce1b514574a9be26632af9
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 20:40:58 +0300

    #4

---
+++ Final Practice Questions/Q4.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int dir_inti(51);
+    int as_inti;
+    int li_inti{34};
+    as_inti=53;
+
+    cout<<"This is set by Direct-Initialization: " <<dir_inti<<endl;
+    cout<<"This is Assigned after Declaration: " <<as_inti<<endl;
+    cout<<"This is List-Initialization: " <<li_inti<<endl;
+
+}
