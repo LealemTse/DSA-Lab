ef3ef2dce49eae7b0698cbb14c860487d7babb44
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 19:27:32 +0300

    updates

---
+++ Final Practice Questions/Q1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"Welcome to C++"<<endl;
+    cout<<"Introduction to C++ languge "<<endl;
+    cout<<"C++ is a powerful and flexible programming language, which was designed to"<<endl;
+    cout<<"build upon the constructs of the C programming language. Thus, with minor"<<endl;
+    cout<<"ex-ceptions, C++ is a superset of the C programming language. C++ shares"<<endl; 
+    cout<<"C's ability to deal efficiently with hardware at the level of bits, bytes, words, addresses, etc."<<endl;
+
+    return 0;
+}
