e58dc83ace7b0bb8e444880e373fca75cc82d104
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-13 21:45:48 +0300

    #8

---
+++ Final Practice Questions/Q8.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+    int ini_inv, new_inv, item_sold;
+    double price, tot_sold;
+    
+
+    cout<<"Enter the Initial Inventory:" <<endl;
+    cin>>ini_inv;
+    cout<<"Enter the Product Price:" <<endl;
+    cin>>price;
+    cout<<"Enter the Number of Item sold:" <<endl;
+    cin>>item_sold;
+
+    new_inv=ini_inv-=item_sold;
+    tot_sold=price*item_sold;
+
+
+
+    cout<<"New Inventory: "<<new_inv<<endl;
+    cout<<"Total Sold: "<<tot_sold<<endl;
+
+    return 0;
+}
