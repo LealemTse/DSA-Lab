7cef83e561927fda84efd3b63ddb8366f7b20bbc
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-15 16:34:32 +0300

    Factorial (for loop)

---
+++ Lab Excersis-1/10.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+ int main(){
+    int n;
+    long f=1L;
+    cout<<"Enter Numebr";
+    cin>>n;
+
+    
+    for (int i = 1; i <= n; ++i)
+    {
+       f=f*i;
+
+    }
+    cout <<f<<endl;
+    return 0;
+    
+
+ }
