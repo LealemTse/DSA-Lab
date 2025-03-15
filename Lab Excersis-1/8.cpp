bf504399572f628bc1bff1f991607e10cce45390
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-15 15:02:03 +0300

    update

--- Lab Excersis-1/8.cpp
+++ Lab Excersis-1/8.cpp
@@ -1,3 +1,24 @@
-// Write a C++ program that uses an if-else statement to check if a number is positive, a
-// switch statement to handle specific values, and a for loop to print a countdown. Briefly
-// explain what each control structure does.
+#include<iostream>
+
+using namespace std;
+
+// void duplicate(int a, int b, int c){
+void duplicate(int& a, int& b, int& c){
+
+    a*=2;
+    b*=2;
+    c*=2;
+   
+}
+int main (){
+    int x=1, y=3, z=7 ;
+    int g;
+    duplicate(x,y,z);
+    g=x+y+z;
+    cout<<"x:"<<x<<"y:"<<y<<"z:"<<z<<"g"<<g<<endl;
+    return 0;
+}
+
+//thie first one shoud out put x=1, y=3 and z=7
+
+// ths second one should output x=2, y=6 and z=14
