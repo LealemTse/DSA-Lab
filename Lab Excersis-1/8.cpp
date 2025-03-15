dd34d421216e29c33b6ca044928c810928e73db0
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-15 15:40:29 +0300

    update

--- Lab Excersis-1/8.cpp
+++ Lab Excersis-1/8.cpp
@@ -1,22 +1,35 @@
 #include<iostream>
-
+#include<string>
 using namespace std;
 
-// void duplicate(int a, int b, int c){
-void duplicate(int& a, int& b, int& c){
+// void duplicate(int a, int b, int c, int d){
+// void duplicate(int& a, int& b, int& c, int& d){
 
-    a*=2;
-    b*=2;
-    c*=2;
-   
+//     a*=2;
+//     b*=2;
+//     c*=2;
+//     d=a+b+c;  
+//    cout<<d<<endl;
+// }
+
+// string cocatinate(const string& t, const string& u){
+inline string cocatinate(string t, string u){
+    return t+u;
 }
 int main (){
-    int x=1, y=3, z=7 ;
-    int g;
-    duplicate(x,y,z);
-    g=x+y+z;
-    cout<<"x:"<<x<<"y:"<<y<<"z:"<<z<<"g"<<g<<endl;
-    return 0;
+    // int x=1, y=3, z=7, g ;
+
+    string j="Hello";
+    string i="World!";
+
+    cocatinate(j,i);
+
+    // duplicate(x,y,z,g);
+    // cout<<"x:"<<x<<"y:"<<y<<"z:"<<z<<endl;
+
+    cout<<cocatinate <<endl;
+    //  return 0;
+
 }
 
 //thie first one shoud out put x=1, y=3 and z=7
