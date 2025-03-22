33985a5c800ea36f0f2cda606230c5a4f7f51a02
Lealem.T.Gete <lealemtsehay9@gmail.com>
2025-03-22 16:09:43 +0300

    Update

--- Lab Works/Arrays_linkedlists.cpp
+++ Lab Works/Arrays_linkedlists.cpp
@@ -21,13 +21,65 @@ Node(int data1){
 }
 
 };
+// the conncetion makers
 Node* convetArr2LL(vector<int> &arr){
-    Node* head =new Node{arr[0]};
+    Node* head =new Node(arr[0]); //creat where the node is in the array
+    Node* moverHead=head;
+    for(int i; i<arr.size();i++){
+        Node* tempNode =new Node(arr[i]);
+        moverHead-> next=tempNode;
+        moverHead=tempNode; 
+    }
+    return head;
+}
+void printList(Node*head){ //to print all the elents of the linked list
+    Node* moverHead = head;
+    while(moverHead!=NULL){
+        cout<<moverHead->data << "->"; 
+        moverHead = moverHead-> next;
+    }
+    cout<<"NULL";
+    cout<<'\n';
+
+    
+} 
+//instering at the first 
+Node* insetHead(Node*head, int value){
+    Node* newNode =new Node(value);
+    newNode->next =head;// if we inter 
+    head=newNode;
+        
+    return head;
+}
+
+//inster to the tail 
+Node* insetTail(Node*head, int value){
+   Node* moverHead = head;
+   while(moverHead->next != NULL){
+    moverHead=moverHead ->next;
+   }
+}
+//inserting at the k places
+Node*insetAtK(Node*head, int value){
     
 }
+
 
 int main(){
     //linked list creation
     vector<int> arr ={1,2,3,4,5};
+    Node* head=convetArr2LL(arr);
+    Node*head2 = insertHead(head, 0);
+    Node*head3 = insetTail(head,6);
+
+//  cout<<head<<endl //this will print the memory address of the data
+    cout<<head->data<<endl; //this will print "1" the head or the fist data in the list only
+    printList(head);
+    printList(head2);
+    printList(head3);
+
+
+
+
 
 }
